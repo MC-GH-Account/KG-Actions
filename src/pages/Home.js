8031e20390ec227ea220a41abd8fad03deb1ff43
MC-GH-Account <93674400+MC-GH-Account@users.noreply.github.com>
2021-11-30 13:26:04 -0500

    make navbar accessible zoom 300 in english

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,43 +26,44 @@ const Home = () => {
   }
 
   return(
-    <Container className="p-3">
-
+    <>
       <Navigation />
+      <Container className="p-3">
 
-      {/*Content*/}
-      <Container className="p-5 mb-4 bg-light rounded-3">
+        {/*Content*/}
+        <Container className="p-5 mb-4 bg-light rounded-3">
 
-        {/*Header*/}
-        <Row className="">
-          <Col>
-            <h1 className="header">{contentTranslations.title}</h1>
-          </Col>
-        </Row>
+          {/*Header*/}
+          <Row className="">
+            <Col>
+              <h1 className="header">{contentTranslations.title}</h1>
+            </Col>
+          </Row>
 
-        {/*Introduction*/}
-        <Row>
-          <p>{contentTranslations.paragraph[0]}</p>
-          <p>{contentTranslations.paragraph[1]}</p>
-        </Row>
+          {/*Introduction*/}
+          <Row>
+            <p>{contentTranslations.paragraph[0]}</p>
+            <p>{contentTranslations.paragraph[1]}</p>
+          </Row>
 
-        {/*Sub Header*/}
-        <Row className="justify-content-md-center">
-          <Col md="auto"><h2 className="display-6 bg-primary rounded-3 p-2">{contentTranslations.subtitle}</h2></Col>
-        </Row>
+          {/*Sub Header*/}
+          <Row className="justify-content-md-center">
+            <Col md="auto"><h2 className="display-6 bg-primary rounded-3 p-2">{contentTranslations.subtitle}</h2></Col>
+          </Row>
 
-        {/*Further Information*/}
-        <Row>
-          <p>{contentTranslations.paragraph[2]}</p>
-          <p>{contentTranslations.paragraph[3]}</p>
-        </Row>
+          {/*Further Information*/}
+          <Row>
+            <p>{contentTranslations.paragraph[2]}</p>
+            <p>{contentTranslations.paragraph[3]}</p>
+          </Row>
 
-        {/*Call To User Action*/}
-        <Row className="justify-content-md-center">
-          <Col md="auto"><h2 className="display-4 bg-light border border-primary rounded-3 p-3"><a href="#call_to_action" className="no-underline">{contentTranslations.callToAction}</a></h2></Col>
-        </Row>
+          {/*Call To User Action*/}
+          <Row className="justify-content-md-center">
+            <Col md="auto"><h2 className="display-4 bg-light border border-primary rounded-3 p-3"><a href="#call_to_action" className="no-underline">{contentTranslations.callToAction}</a></h2></Col>
+          </Row>
+        </Container>
       </Container>
-    </Container>
+    </>
   );
 }
 
