eb023604b32bf32f643a1aa422dd39b5ac6b0730
MC-GH-Account <93674400+MC-GH-Account@users.noreply.github.com>
2021-11-30 08:07:24 -0500

    init commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
+import {IntlProvider} from 'react-intl';
+import Wrapper from "./Wrapper";
+import "./index.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+	<Wrapper>
+		<App />
+	</Wrapper>,
+	document.getElementById("root")
+);
